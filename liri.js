40e4d160c3d4fce47fce57380e6d05f3f238fdb5
Bill Kamberos <bill.kamberos@gmail.com>
2020-03-03 11:47:58 -0500

    Spotify function added

--- liri.js
+++ liri.js
@@ -69,3 +69,85 @@ var concertThis = function(input) {
       console.log('**No Artist Found**');
     });
 };
+
+//SPOTIFY FUNCTION
+var spotifyThisSong = function(input) {
+  spotify
+    .search({ type: 'track', query: input })
+    .then(function(response) {
+      if (response.tracks.items && response.tracks.items.length) {
+        fs.appendFile(
+          'log.txt',
+          `SPOTIFY SEARCH RESULTS FOR ${input.toUpperCase()}\n`,
+          function(err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+        response.tracks.items.forEach((item, idx) => {
+          if (item.preview_url === null) {
+            var preview = 'No Preview Available';
+          } else {
+            var preview = item.preview_url;
+          }
+          var count = idx + 1;
+          console.log(
+            'Result: #' +
+              count +
+              '\nArtist: ' +
+              item.artists[0].name +
+              '\nSong name: ' +
+              item.name +
+              '\nPreview URL: ' +
+              preview +
+              '\nAlbum Name: ' +
+              item.album.name +
+              '\n'
+          );
+
+          fs.appendFile(
+            'log.txt',
+            `----------\nResult#: ${count} \nArtist: ${item.artists[0].name} \nSong Name: ${item.name} \nPreviewURL: ${preview} \nAlbum Name: ${item.album.name}\n`,
+            function(err) {
+              if (err) {
+                console.log(err);
+              }
+            }
+          );
+        });
+        fs.appendFile('log.txt', `\n \n \n`, function(err) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      } else {
+        console.log(
+          "**Sorry, that song wasn't found. Here's 'The Sign' by Ace of Base.**"
+        );
+        spotify
+          .search({ type: 'track', query: 'The Sign' })
+          .then(function(response) {
+            console.log(response.tracks.items[0].artists[0].name);
+            console.log(response.tracks.items[0].name);
+            console.log(response.tracks.items[0].preview_url);
+            console.log(response.tracks.items[0].album.name);
+          });
+      }
+    })
+    .catch(function(err) {
+      console.log(
+        "**Sorry, that song wasn't found. Here's 'The Sign' by Ace of Base.**"
+      );
+      spotify
+        .search({ type: 'track', query: 'The Sign' })
+        .then(function(response) {
+          console.log(response.tracks.items[0].artists[0].name);
+          console.log(response.tracks.items[0].name);
+          console.log(response.tracks.items[0].preview_url);
+          console.log(response.tracks.items[0].album.name);
+        });
+
+      console.log(err);
+    });
+};
